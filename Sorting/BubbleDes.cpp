aa1e8257c86acb56d23ab27dddf5dbc87ebd1f84
Sifat Ullah <usifat59@gmail.com>
2024-09-17 20:26:57 +0600

    descending

---
+++ Sorting/BubbleDes.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSortDescending(int arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << " " << arr[i];
+    cout << endl;
+}
+
+int main()
+{
+    int N;
+    cout << "Enter the number of elements: ";
+    cin >> N;
+    
+    int arr[N];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSortDescending(arr, N);
+    
+    cout << "Sorted array in descending order: \n";
+    printArray(arr, N);
+
+    return 0;
+}
