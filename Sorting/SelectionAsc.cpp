126a7ebc6946685648174aa3f574c139ed820f39
Sifat Ullah <usifat59@gmail.com>
2024-09-18 10:54:47 +0600

    ascending

---
+++ Sorting/SelectionAsc.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, n);
+    
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+
+    return 0;
+}
