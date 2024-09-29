c7bb05b0cb4f192f94f352dece84f38ba670a9ed
Sifat Ullah <usifat59@gmail.com>
2024-09-29 09:43:49 +0600

    printing E

---
+++ Pattern/E.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if ( (i == 2 && (j == 2 || j == 3 || j == 4 || j == 5)) || (i == 4 && (j == 2 || j == 3 || j == 4  || j == 5) )){
+                cout << "  ";
+            }
+            else {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
