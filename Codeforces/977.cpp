4bb09e4d58ff96b69be7fff6b54db6edb761a05a
Sifat Ullah <usifat59@gmail.com>
2025-01-08 17:04:19 +0600

    Added solution for Codeforces 977A - Wrong Subtraction
    - Implemented a solution in C++ to reduce the given number `n` by `m` steps.
    - Each step decrements `n` by 1 unless its last digit is zero, in which case it divides `n` by 10.
    - Outputs the final value of `n` after `m` steps.

---
+++ Codeforces/977.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++)
+    {
+        if(n%10==0)
+        {
+            n /= 10;
+        }
+        else 
+        {
+            n--;
+        }  
+    }
+    cout<<n;
+}
