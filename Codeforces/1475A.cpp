68951f3948a444352d2c2730fa399da8db51c4d2
Sifat Ullah <usifat59@gmail.com>
2025-01-10 00:15:52 +0600

    Added solution for Codeforces Problem 1475A Odd Divisor
    - Implemented a solution to determine if a number `n` has an odd divisor greater than 1.
    - Utilized bitwise operations to check if `n` is not a power of 2.
    - Input: Number of test cases `t` followed by `t` integers.
    - Output: "YES" if `n` has an odd divisor greater than 1, otherwise "NO".
    - Efficient solution with O(t) complexity.

---
+++ Codeforces/1475A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+
+        if (n & (n - 1))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
