5e47b91dfe386c590da8006148e61c48d1d3487f
Sifat Ullah <usifat59@gmail.com>
2025-01-14 18:46:12 +0600

    Added solution for Codeforces 58A - Chat room
    - Implemented a program to check if the string "hello" appears as a subsequence in the input string.
    - Used a pointer to track progress through the target string "hello".
    - Output "YES" if the sequence is found, otherwise "NO".

---
+++ Codeforces/58A.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    string target="hello";
+    int j=0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == target[j]) {
+            j++;
+        }
+        if (j == target.length()) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+    return 0;
+
+}
