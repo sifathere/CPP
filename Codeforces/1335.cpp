0e55ef723a6bc78e7bfbbc910de974649fc25898
Sifat Ullah <usifat59@gmail.com>
2025-02-16 10:23:49 +0600

    Added solution for Codeforces 1335A - Candies and Two Sisters
    Implemented a simple approach using integer division.
    Read test cases and computed the maximum number of candies one sister can get.
    Used (n-1)/2 formula for optimal calculation.

---
+++ Codeforces/1335.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    int n;
+    for(int i=0;i<t;i++)
+    {
+        cin>>n;
+        cout<<(n-1)/2<<endl;
+    }
+}
