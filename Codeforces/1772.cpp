3ba190d36d54f4f3731f627b74ec1c4a700b7755
Sifat Ullah <usifat59@gmail.com>
2025-02-27 22:18:04 +0600

    Add solution for Codeforces 1772A
    - Implemented a simple arithmetic parser for "A + B" format.
    - Reads input values, extracts integers, and computes their sum.
    - Uses a loop to handle multiple test cases.
    
    Problem: https://codeforces.com/problemset/problem/1772/A

---
+++ Codeforces/1772.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        char c;
+        cin>>a>>c>>b;
+        cout<<a+b<<endl;
+    }
+}
