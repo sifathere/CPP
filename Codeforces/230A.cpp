8f694fd7e9fc0ffa218078695a1f0b882a2ddc44
Sifat Ullah <usifat59@gmail.com>
2025-03-13 22:05:58 +0600

    Add solution for Codeforces 230A - Dragons
    - Implemented a greedy algorithm to determine if the hero can defeat all dragons.
    - Used sorting to ensure the hero fights weaker dragons first.
    - Checked each dragon sequentially, updating strength upon victory.
    - Outputs "YES" if all dragons are defeated, otherwise "NO".
    
    Time Complexity: O(n log n) due to sorting.

---
+++ Codeforces/230A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int s, n;
+    cin >> s >> n;
+    vector<pair<int, int>> dragons(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> dragons[i].first >> dragons[i].second;
+    }
+
+    sort(dragons.begin(), dragons.end());
+
+    for (auto &d : dragons) {
+        if (s > d.first) {
+            s += d.second;
+        } else {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+
+    cout << "YES" << endl;
+    return 0;
+}
